6ad027f84437642f25f9b00e96aa6169b03f1d69
Ederhipraise <ederhifejidar@gmail.com>
2023-06-19 15:21:44 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - we are writing a command that prints a setence on a new line without using the printf command
+ *
+ * return: Always 0 (success)
+ */
+
+int main() 
+	{
+		puts("Programming is like building a multilingual puzzle");
+		return (0);
+	}
