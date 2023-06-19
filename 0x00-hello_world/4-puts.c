34ca12dc1cda4b1182080dbdce5fe82a7b2069f7
Ederhipraise <ederhifejidar@gmail.com>
2023-06-19 16:39:47 -0700

    5-printf.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /**
- * main - we are writing a command that prints a setence on a new line without using the printf command
+ * main - Entry point
  *
- * return: Always 0 (success)
+ * Return: 0 (Success)
  */
-
-int main() 
-	{
-		puts("Programming is like building a multilingual puzzle");
-		return (0);
-	}
+int main(void) 
+{
+putchar("\"Programming is like building a multilingual puzzle");
+return (0);
+}
