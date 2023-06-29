6b4450893206fb443a0f075beac799a03c428cb7
Ederhipraise <ederhifejidar@gmail.com>
2023-06-29 10:25:28 -0700

    printing single numbers well spaced with comma

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int number = '0';
+
+	for (number = 0; number < 10; number++)
+	{
+		putchar(number + '0');
+		if (number != 10)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
