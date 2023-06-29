7950195173ddeea7c707cdcd760513f44b35989f
Ederhipraise <ederhifejidar@gmail.com>
2023-06-29 08:28:29 -0700

    printing in lowercase with exception to some alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char letter = 'A';
+
+	while (letter <= 'Z')
+	{
+		if (letter == 'Q' || letter == 'E')
+		{
+			putchar(letter);
+		}	else
+		{
+			putchar(tolower(letter));
+		}
+		letter++;
+	}
+	putchar('\n');
+	return (0);
+}
