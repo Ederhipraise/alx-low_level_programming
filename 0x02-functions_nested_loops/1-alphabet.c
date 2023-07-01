5323e7bf2060bdb82a66efa1c952db1817a69e76
Ederhipraise <ederhifejidar@gmail.com>
2023-07-01 13:40:57 -0700

    printing alpahbet with ASCII

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+void main(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
