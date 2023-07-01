c401145046d9349f5d3a9d8f2e9e1c1b422617d5
Ederhipraise <ederhifejidar@gmail.com>
2023-07-01 13:19:45 -0700

    printing _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,6 +6,15 @@
  */
 int main(void)
 {
-	printf("_putchar\n");
+	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
+	int i, a;
+	
+	a = sizeof(str) / sizeof(int);
+
+	for (i = 0; i < a; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
 	return (0);
 }
